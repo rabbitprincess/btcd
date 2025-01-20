3baa57ccbe6372dc33b9ef12bb1b6db6355687a3
Kim <dreamcacao02183@gmail.com>
2025-01-20 21:26:44 +0900

    print correct dbtype

--- database/ffldb/driver.go
+++ database/ffldb/driver.go
@@ -20,23 +20,23 @@ const (
 )
 
 // parseArgs parses the arguments from the database Open/Create methods.
-func parseArgs(funcName string, args ...interface{}) (string, wire.BitcoinNet, error) {
+func parseArgs(dbType, funcName string, args ...interface{}) (string, wire.BitcoinNet, error) {
 	if len(args) != 2 {
 		return "", 0, fmt.Errorf("invalid arguments to %s.%s -- "+
-			"expected database path and block network", LevelDB,
+			"expected database path and block network", dbType,
 			funcName)
 	}
 
 	dbPath, ok := args[0].(string)
 	if !ok {
 		return "", 0, fmt.Errorf("first argument to %s.%s is invalid -- "+
-			"expected database path string", LevelDB, funcName)
+			"expected database path string", dbType, funcName)
 	}
 
 	network, ok := args[1].(wire.BitcoinNet)
 	if !ok {
 		return "", 0, fmt.Errorf("second argument to %s.%s is invalid -- "+
-			"expected block network", LevelDB, funcName)
+			"expected block network", dbType, funcName)
 	}
 
 	return dbPath, network, nil
@@ -45,7 +45,7 @@ func parseArgs(funcName string, args ...interface{}) (string, wire.BitcoinNet, e
 // openDBDriver is the callback provided during driver registration that opens
 // an existing database for use.
 func openDBDriver(dbType string, args ...interface{}) (database.DB, error) {
-	dbPath, network, err := parseArgs("Open", args...)
+	dbPath, network, err := parseArgs(dbType, "Open", args...)
 	if err != nil {
 		return nil, err
 	}
@@ -56,7 +56,7 @@ func openDBDriver(dbType string, args ...interface{}) (database.DB, error) {
 // createDBDriver is the callback provided during driver registration that
 // creates, initializes, and opens a database for use.
 func createDBDriver(dbType string, args ...interface{}) (database.DB, error) {
-	dbPath, network, err := parseArgs("Create", args...)
+	dbPath, network, err := parseArgs(dbType, "Create", args...)
 	if err != nil {
 		return nil, err
 	}
