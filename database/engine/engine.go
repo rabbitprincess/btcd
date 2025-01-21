fb55cd6c7896dbe213bed091fc46f8ea12752508
Kim <dreamcacao02183@gmail.com>
2025-01-21 11:01:18 +0900

    unify unnecessary files

--- database/engine/engine.go
+++ database/engine/engine.go
@@ -12,3 +12,14 @@ type Transaction interface {
 	Commit() error
 	Discard()
 }
+
+type Snapshot interface {
+	Get(key []byte) ([]byte, error)
+	Has(key []byte) (bool, error)
+	NewIterator(start, end []byte) (Iterator, error)
+	Releaser
+}
+
+type Releaser interface {
+	Release()
+}
